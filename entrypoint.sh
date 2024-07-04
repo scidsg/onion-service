a825ca2c9a6560ec11e9f542252bfb4fe692b651
Micah Lee <micah@micahflee.com>
2024-07-04 12:52:44 -0700

    Start a SOCKS5 service listening on 9050, and add some debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Debug output
+echo "ONION_HOSTNAME: $ONION_HOSTNAME"
+echo "ONION_PUBLIC_KEY_B64: $ONION_PUBLIC_KEY_B64"
+echo "ONION_SECRET_KEY_B64: <redacted>"
+echo "ONION_PORT: $ONION_PORT"
+
 # Configure the onion service
 mkdir -p /var/lib/tor/onion_service
 echo $ONION_HOSTNAME > /var/lib/tor/onion_service/hostname
@@ -9,7 +15,8 @@ chown -R debian-tor:debian-tor /var/lib/tor/onion_service
 chmod 700 /var/lib/tor/onion_service
 
 # Configure torrc
-echo "HiddenServiceDir /var/lib/tor/onion_service" > /etc/tor/torrc
+echo "SocksPort 0.0.0.0:9050" > /etc/tor/torrc
+echo "HiddenServiceDir /var/lib/tor/onion_service" >> /etc/tor/torrc
 echo "HiddenServicePort 80 $ONION_PORT" >> /etc/tor/torrc
 
 # Start tor
